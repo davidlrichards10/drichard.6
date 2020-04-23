1da6acfc93007667667fc5e01747b5a7566da25f
davidlrichards10 <62032455+davidlrichards10@users.noreply.github.com>
2020-04-23 10:12:16 -0500

    Create user.c

---
+++ user.c
@@ -0,0 +1,19 @@
+/* Author: David Richards
+ * Date: Thurs April 23rd
+ * Assignment: CS4760 hw6
+ * File: user.c
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include "shared.h"
+
+int main(int argc, char** argv) 
+{
+
+return 0;
+}
