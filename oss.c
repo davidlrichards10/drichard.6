ac3cb3d84abd618f4615608cc91f54cebeefe884
davidlrichards10 <62032455+davidlrichards10@users.noreply.github.com>
2020-04-23 10:10:48 -0500

    Create oss.c

---
+++ oss.c
@@ -0,0 +1,23 @@
+/* Author: David Richards
+ * Date: Thurs April 23rd
+ * Assignment: CS4760 hw6
+ * File: oss.c
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/msg.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include "shared.h"
+
+int main(int argc, char* argv[]) 
+{
+
+
+return 0;
+}
